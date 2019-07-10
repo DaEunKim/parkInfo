92ba2c7dd7fbc8be0a8e29443b9e43bf1ee7da30
DaEunKim <ekdms717@naver.com>
2019-07-10 16:47:45 +0900

    add tabs function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import calculRound from "../components/calculRound";
-import useAxios from "../hooks/useAxios";
+// import useAxios from "../hooks/useAxios";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 
 const cityList = [
   { id: 1, value: "Seoul" },
@@ -40,9 +41,8 @@ export default function Home() {
 
   const weatherApi = `http://api.openweathermap.org/data/2.5/weather?q=${CityNameAPI}&APPID=${APPID}`;
 
-  const data2 = useAxios(weatherApi);
+  // const data2 = useAxios(weatherApi);
 
-  console.log("data2 : ", data2);
   useEffect(() => {
     const fetchData = async () => {
       setIsError(false);
@@ -61,48 +61,64 @@ export default function Home() {
   if (!data) {
     return <></>;
   }
-  // console.log(data);
+
   return (
     <>
-      <p>도시를 고르시오</p>
-      {isError && <>error</>}
-      {isLoading ? (
-        <>loading...</>
-      ) : (
-        <select
-          name="city"
-          value={cityName}
-          onChange={e => setCityName(e.target.value)}
-        >
-          {cityListKR.map(city => (
-            <option key={city.id} value={city.id}>
-              {city.value}
-            </option>
-          ))}
-        </select>
-      )}
-      <br />
-      <br />
+      <Tabs>
+        <TabList>
+          <Tab>지역별 날씨</Tab>
+          <Tab>지도</Tab>
+          <Tab>Title 3</Tab>
+        </TabList>
 
-      <div>{`위치 : ${data.name} / ${data.sys && data.sys.country}`}</div>
-      <br />
-      <div>
-        {`현재 기온 : ${data.main && calculRound(data.main.temp - 273.15)}°C`}
-      </div>
-      <div>{`날씨 상태 : ${data.weather && data.weather[0].description}`}</div>
-      <div>{`풍속 : ${data.wind && data.wind.speed}m/s`}</div>
-      <div>{`구름 : ${data.clouds && data.clouds.all}%`}</div>
-      <div>{`현재 습도 : ${data.main && data.main.humidity}%`}</div>
-      <div>{`기압 : ${data.main && data.main.pressure}hPa`}</div>
-      <br />
-      <div>
-        {`최고 기온 : ${data.main &&
-          calculRound(data.main.temp_max - 273.15)}°C`}
-      </div>
-      <div>
-        {`최소 기온 : ${data.main &&
-          calculRound(data.main.temp_min - 273.15)}°C`}
-      </div>
+        <TabPanel>
+          <p>도시를 고르시오</p>
+          {isError && <>error</>}
+          {isLoading ? (
+            <>loading...</>
+          ) : (
+            <select
+              name="city"
+              value={cityName}
+              onChange={e => setCityName(e.target.value)}
+            >
+              {cityListKR.map(city => (
+                <option key={city.id} value={city.id}>
+                  {city.value}
+                </option>
+              ))}
+            </select>
+          )}
+          <br />
+          <br />
+          <div>{`위치 : ${data.name} / ${data.sys && data.sys.country}`}</div>
+          <div>
+            {`현재 기온 : ${data.main &&
+              calculRound(data.main.temp - 273.15)}°C`}
+          </div>
+          <div>{`날씨 상태 : ${data.weather &&
+            data.weather[0].description}`}</div>
+          <div>{`풍속 : ${data.wind && data.wind.speed}m/s`}</div>
+          <div>{`구름 : ${data.clouds && data.clouds.all}%`}</div>
+          <div>{`현재 습도 : ${data.main && data.main.humidity}%`}</div>
+          <div>{`기압 : ${data.main && data.main.pressure}hPa`}</div>
+          <br />
+          <div>
+            {`최고 기온 : ${data.main &&
+              calculRound(data.main.temp_max - 273.15)}°C`}
+          </div>
+          <div>
+            {`최소 기온 : ${data.main &&
+              calculRound(data.main.temp_min - 273.15)}°C`}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 3</h2>
+        </TabPanel>
+      </Tabs>
     </>
   );
 }
