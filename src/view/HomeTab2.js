3c7b0e417272565a870e5c2117ab5598a563626f
DaEunKim <ekdms717@naver.com>
2019-07-17 09:38:22 +0900

    modify post

--- src/view/HomeTab2.js
+++ src/view/HomeTab2.js
@@ -4,51 +4,51 @@ import { callNative } from "../components/CommonFunction";
 import useAxios from "../hooks/useAxios";
 import axios from "axios";
 
-function followClick(
-  e,
-  isClicked,
-  setBookmarkState,
-  title,
-  setTitle,
-  creator,
-  setCreator,
-  content,
-  setContent
-) {
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const postAPI = `${proxyUrl}http://lunahc92.tplinkdns.com/api/posts/create`;
+// function followClick(
+//   e,
+//   isClicked,
+//   setBookmarkState,
+//   title,
+//   setTitle,
+//   creator,
+//   setCreator,
+//   content,
+//   setContent
+// ) {
+//   const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+//   const postAPI = `${proxyUrl}http://lunahc92.tplinkdns.com/api/posts/create`;
 
-  // const bookmarkUrl = `${API_URL}/api/gdny/v1/users/${id}/bookmarks`;
-  // const query = queryString.parse(search);
-  e.preventDefault();
+//   // const bookmarkUrl = `${API_URL}/api/gdny/v1/users/${id}/bookmarks`;
+//   // const query = queryString.parse(search);
+//   e.preventDefault();
 
-  if (isClicked) {
-    setBookmarkState(false);
-    setTitle(false);
-    setCreator(false);
-    setContent(false);
-    return axios({
-      method: "delete",
-      url: postAPI
-    });
-  }
+//   // if (isClicked) {
+//   //   setBookmarkState(false);
+//   //   setTitle(false);
+//   //   setCreator(false);
+//   //   setContent(false);
+//   //   return axios({
+//   //     method: "delete",
+//   //     url: postAPI
+//   //   });
+//   // }
 
-  return axios({
-    url: postAPI,
-    method: "post"
-    // data: { title: { title }, creator: { creator }, text: { content } }
-  }).then(response => {
-    if (response.data.meta.status === "error") {
-      alert(response.data.meta.alert_message);
-    } else {
-      setBookmarkState(true);
-      setTitle(true);
-      setCreator(true);
-      setContent(true);
-    }
-  });
-}
-const useFetchData = ({ url, headers, payload }) => {
+//   return axios({
+//     url: postAPI,
+//     method: "post"
+//     // data: { title: { title }, creator: { creator }, text: { content } }
+//   }).then(response => {
+//     if (response.data.meta.status === "error") {
+//       alert(response.data.meta.alert_message);
+//     } else {
+//       setBookmarkState(true);
+//       setTitle(true);
+//       setCreator(true);
+//       setContent(true);
+//     }
+//   });
+// }
+const useFetchData = ({ url, data, headers, payload }) => {
   const [res, setRes] = useState({ data: null, error: null, isLoading: false });
   const [error, setError] = useState(0);
 
@@ -57,7 +57,8 @@ const useFetchData = ({ url, headers, payload }) => {
     axios
       .post(url, headers, payload)
       .then(res => {
-        console.log(res.data.post.title);
+        console.log(res);
+
         // console.log(res.data.content.post.title);
         setRes({ data: res.data, isLoading: false, error: null });
       })
@@ -78,7 +79,6 @@ export default function HomeTab2({ Props }) {
     payload: {}
   });
 
-  const [follow, setFollow] = useState(0);
   const [title, setTitle] = useState("");
   const [creator, setCreator] = useState("");
   const [content, setContent] = useState("");
@@ -92,36 +92,17 @@ export default function HomeTab2({ Props }) {
     setContent(e.target.value);
   };
 
-  // const onChangePost = useCallback(
-  //   (e, followed, title, creator, content) => {
-  //     e.preventDefault();
-  //     followClick(
-  //       e,
-  //       followed,
-  //       setFollow,
-  //       title,
-  //       setTitle,
-  //       creator,
-  //       setCreator,
-  //       content,
-  //       setContent
-  //     // );
-  //   },
-  //   [follow]
-  // );
-
-  // const postAPI = `http://lunahc92.tplinkdns.com/api/posts/create`;
-  // const { data, isLoading, isError } = useAxios({
-  //   url: `${postAPI}`,
-  //   method: "post"
-  // });
-  // console.log(data);
   return (
     <>
       <ul key={`${tabIndex}_ul2`}>
         <li key={`${tabIndex}_li2`}>
           <div>글쓰기</div>
-          <form>
+          <form
+          // onSubmit={e => {
+          //   e.preventDefault();
+          //   apiMethod();
+          // }}
+          >
             <input
               type="text"
               placeholder="title"
@@ -156,12 +137,9 @@ export default function HomeTab2({ Props }) {
             </div>
 
             <button
-              type="button"
               onClick={e => {
                 e.preventDefault();
-                apiMethod(title, creator, content);
-                // onChangePost(e, follow, title, creator, content);
-                // callNative({ pageName: "board", parameter: {} });
+                apiMethod();
               }}
             >
               Submit
