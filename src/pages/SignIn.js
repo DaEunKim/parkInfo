3a7c4818aaa641321cd9c92f689b1fc15190b927
DaEunKim <ekdms717@naver.com>
2019-07-29 20:31:47 +0900

    add sign up page and sign in page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,1 +1,17 @@
 // 로그인
+
+// 최상단 페이지
+// 로그인 페이지 -> 회원이면 로그인, 비회원이면 회원가입 페이지
+import React, { useState } from "react";
+import "./style-narrower.css";
+import queryString from "query-string";
+import useAxios from "../hooks/useAxios";
+import { HANUL_API, PROXY_URL } from "../CONSTANTS/url";
+
+export default function SignIn({ location: { search } }) {
+  const query = queryString.parse(search);
+  console.log(query);
+  const { creator, id, time, title } = query;
+
+  return <>dd</>;
+}
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,26 +1,118 @@
 // 회원가입 페이지
-import React from "react";
-import useAxios from "../hooks/useAxios";
-import queryString from "query-string";
-
+import React, { useState } from "react";
+import "./style-narrower.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 import { HANUL_API, PROXY_URL } from "../CONSTANTS/url";
+import useAxios from "../hooks/useAxios";
+
+function checkedID(id) {
+  const SignInUrl = `${PROXY_URL}${HANUL_API}:5100/api/users/read/${id}`;
+  axios
+    .get(SignInUrl)
+    .then(res => {
+      if (res.data.success) {
+        return alert("사용할 수 없는 아이디입니다. ");
+      }
+      return alert("사용할 수 있는 아이디입니다. ");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
 export default function SignUp({ location: { search } }) {
-  //  http://lunahc92.tplinkdns.com:5100/api/users/create
   const SignUpUrl = `${PROXY_URL}${HANUL_API}:5100/api/users/create`;
+
+  const [id, setId] = useState("");
+
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [checkRes, setCheckRes] = useState();
+
+  //   const checkedID = 0;
+  //   if (getSignIn.data.success) {
+  //     checkedID = getSignIn.data.user.id;
+  //   }
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!id) {
+      return alert("아이디를 입력하세요");
+    }
+    if (!password) {
+      return alert("비번을 입력하세요");
+    }
+    if (!name) {
+      return alert("이름을 입력하세요");
+    }
+    if (!email) {
+      return alert("이메일을 입력하세요");
+    }
+
+    const data = {
+      id,
+      password,
+      name,
+      email
+    };
+
+    axios
+      .post(SignUpUrl, data)
+      .then(res => {
+        if (res.data.success) {
+          console.log(res);
+          setCheckRes(res.data);
+
+          // window.location = "./signin";
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
-      <button
-        className="btn-post"
-        onClick={e => {
-          e.preventDefault();
-          window.location = "./board";
-        }}
-      >
-        글쓰기
-      </button>
-      <br />
-      <br />
+      <div className="writing-board-whole">
+        <h1 className="titleBar">회원가입</h1>
+        <input
+          value={id}
+          placeholder="id"
+          onChange={e => setId(e.target.value)}
+        />
+        <button
+          onClick={e => {
+            e.preventDefault();
+            checkedID(id);
+          }}
+        >
+          아이디 확인
+        </button>
+        <br />
+        <input
+          value={password}
+          placeholder="password"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <br />
+        <input
+          value={name}
+          placeholder="name"
+          onChange={e => setName(e.target.value)}
+        />
+        <br />
+        <input
+          value={email}
+          placeholder="email"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <br />
+        <button className="save-button" onClick={handleSubmit}>
+          가입하기
+        </button>
+        <div>{checkRes && "가입완료!"}</div>
+      </div>
     </>
   );
 }
